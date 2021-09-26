06a4f6733804e35a7f0a912a87567beedb3c580f
Sergey Nazhestkin <9497020@mail.ru>
2021-09-26 20:42:40 +0300

    Создан UI Windows Form, реализовано добавление Товаров, Продавцов и Покупателей через UI.

---
+++ CRMUI/Main.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CRMBusinessLogic.Model;
+
+namespace CRMUI
+{
+    public partial class Main : Form
+    {
+        CRMContext db;
+        public Main()
+        {
+            InitializeComponent();
+            db = new CRMContext();
+        }
+        private void ProductStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            var catalogProduct = new Catalog<Product>(db.Products);
+            catalogProduct.Show();
+        }
+
+        private void SellerStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            var catalogSeller = new Catalog<Seller>(db.Sellers);
+            catalogSeller.Show();
+        }
+
+        private void CustomerStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            var catalogCustomer = new Catalog<Customer>(db.Customers);
+            catalogCustomer.Show();
+        }
+
+        private void CheckStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            var catalogCheck = new Catalog<Check>(db.Checks);
+            catalogCheck.Show();
+        }
+
+        private void SellerAddToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            var form = new SellerForm();
+            if (form.ShowDialog() == DialogResult.OK)
+            {
+                db.Sellers.Add(form.Seller);
+                db.SaveChanges();
+            }
+        }
+
+        private void CustomerAddToolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            var form = new CustomerForm();
+            if (form.ShowDialog() == DialogResult.OK)
+            {
+                db.Customers.Add(form.Customer);
+                db.SaveChanges();
+            }
+        }
+
+        private void ProductAddToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var form = new ProductForm();
+            if (form.ShowDialog() == DialogResult.OK)
+            {
+                db.Products.Add(form.Product);
+                db.SaveChanges();
+            }
+        }
+    }
+}
