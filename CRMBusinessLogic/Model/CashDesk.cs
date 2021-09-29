6bd6bcefe1432cfdcbdb592b50eaa04362180aab
Sergey Nazhestkin <9497020@mail.ru>
2021-09-29 22:08:25 +0300

    Добавлена сущность CashDesk

---
+++ CRMBusinessLogic/Model/CashDesk.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CRMBusinessLogic.Model
+{
+    public class CashDesk
+    {
+
+        public int Number { get; set; }
+        public Seller Seller { get; set; }
+        public Queue<Cart> Queue { get; set; }
+        public int MaxQueueLenght { get; set; }
+        public int ExitCustomer { get; set; }
+        public CashDesk(int number, Seller seller)
+        {
+            Number = number;
+            Seller = seller;
+            Queue = new Queue<Cart>();
+        }
+
+        public void Enqueue(Cart cart)
+        {
+            if (Queue.Count <= MaxQueueLenght)
+            {
+                Queue.Enqueue(cart);
+            }
+            else
+            {
+                ExitCustomer++;
+            }
+        }
+
+        public void Dequeue()
+        {
+            var card = Queue.Dequeue();
+            if (card != null)
+            {
+                var check = new Check()
+                {
+                    SellerId = Seller.SellerId,
+                    Seller = Seller,
+                    CustomerId = card.Customer.CustomerId,
+                    Customer = card.Customer,
+                    Created = DateTime.Now
+                };
+            }
+        }
+    }
+}
